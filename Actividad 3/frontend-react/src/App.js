a7e0f0eadd7d3d81d5a7d51b293b86186a794efa
NeonNecron <brusithxd@gmail.com>
2026-02-18 17:57:02 -0600

    Update App.js

--- Actividad 3/frontend-react/src/App.js
+++ Actividad 3/frontend-react/src/App.js
@@ -1,35 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { useState } from 'react';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Tareas from './pages/Tareas';
+import Inventario from './pages/Inventario';
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem('token'));
-
-  const handleLogin = (newToken) => {
-    localStorage.setItem('token', newToken);
-    setToken(newToken);
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    setToken(null);
-  };
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login onLogin={handleLogin} />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/tareas"
-          element={token ? <Tareas onLogout={handleLogout} /> : <Navigate to="/login" />}
-        />
-        <Route path="*" element={<Navigate to="/tareas" />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Inventario />;
 }
 
 export default App;
