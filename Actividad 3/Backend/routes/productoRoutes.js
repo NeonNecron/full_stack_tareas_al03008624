e46da2e6cb690ccff29a833c8716d08bc36448e2
NeonNecron <brusithxd@gmail.com>
2026-02-18 17:52:49 -0600

    Add files via upload

---
+++ Actividad 3/Backend/routes/productoRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const {
+  obtenerProductos,
+  crearProducto,
+  actualizarProducto,
+  eliminarProducto
+} = require('../config/controllers/productoController');
+
+const router = express.Router();
+
+router.get('/', obtenerProductos);
+router.post('/', crearProducto);
+router.put('/:id', actualizarProducto);
+router.delete('/:id', eliminarProducto);
+
+module.exports = router;
